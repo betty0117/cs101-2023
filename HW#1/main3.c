140b61855a24945cd835abe9eeebf8504a58a25e
betty0117 <119960996+betty0117@users.noreply.github.com>
2023-03-02 02:38:24 +0800

    Update main3.c

--- HW#1/main3.c
+++ HW#1/main3.c
@@ -1,24 +1,17 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include<string.h>
 int main()
 {
     char a[]="ABC123";
     int shift=4;
+    int n=sizeof(a)-2;
     for(int z=0;z<shift;z++){
     char tmp=a[0];
-    for(int i=0;i<5;i++){
+    for(int i=0;i<n;i++){
         a[i]=a[i+1];
     
     }
-    a[5]=tmp;
+    a[n]=tmp;
     
     }
     //print
