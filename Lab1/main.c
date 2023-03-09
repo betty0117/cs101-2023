ce68f15f5135d09b7f0eff1f08f7ba2b424fe9ac
betty0117 <119960996+betty0117@users.noreply.github.com>
2023-03-09 14:09:48 +0800

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -4,12 +4,17 @@
 int main()
 {
     FILE* fp;
-    fp=fopen("a.bin","wb+");
+    
     int a[]={1,2,3};
     char b[]="ABC";
     float c[]={1.1,1.2,1.3};
+    int d[3];
+    fp=fopen("a.bin","wb+");
+    fwrite(a,sizeof(int),3,fp);
+    fseek(fp,0,SEEK_SET);
+    fread(d,sizeof(int),3,fp);
     for(int i=0;i<=2;i++){
-        fprintf(fp,"%d ",a[i]);
+        printf("%d",d[i]);
     }
     fclose(fp);
 
