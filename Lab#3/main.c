5e6399a60cdb25865a5d5c675eeac31538134947
betty0117 <119960996+betty0117@users.noreply.github.com>
2023-03-16 14:54:52 +0800

    Create main.c

---
+++ Lab#3/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+typedef struct person {
+	int id;
+	char first_name[32];
+	char last_name[32];
+}person_t;
+
+
+int main(){
+	FILE*rfp=fopen("person.bin","r");
+	//FILE*wfp;
+	person_t tmp[1];
+	int i=0;
+	while(fread(&tmp, sizeof(person_t),1,rfp)){
+		printf("[%d] %d: %s %s\n",i,
+									tmp[0].id,
+									tmp[0].first_name,
+									tmp[0].last_name);
+		i++;
+	}
+	fclose(rfp);
+
+	fclose(rfp);
+}
